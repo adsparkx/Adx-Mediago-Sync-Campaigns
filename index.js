086908fedb30fa9b9ca2c0aa2decf9f354ed3944
dinesh-adsparkx <109131390+dinesh-adsparkx@users.noreply.github.com>
2023-09-01 12:11:43 +0530

    cron changes to preprod

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT;
 const app = express();
-// const { mongoDB } = require("./config/mongo");
-// mongoDB();
-// const cron = require("./cron/cron");
-// cron.initializeCron();
+const { mongoDB } = require("./config/mongo");
+mongoDB();
+const cron = require("./cron/cron");
+cron.initializeCron();
 // app.listen(port, () => {
 //   console.log("listening on http://localhost:" + port);
 // });
